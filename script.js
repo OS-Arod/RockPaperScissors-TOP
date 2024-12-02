17341859e77e83605aa33a689c28b78145c6cd37
HK <hk@HK.local>
2024-12-01 21:08:40 -0800

    Implement playRound function for Rock-Paper-Scissors game logic

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ let computerScore = 0;
 function getComputerChoice() {
     let Choice = Math.floor(Math.random() * 3);
 
-    switch ((Choice)) {
+    switch (Choice) {
         case 0:
+            console.log("Super Smart AI Choose....\n" + "Rock");
             return "Rock"
-            break;
         case 1:
+            console.log("Super Smart AI Choose....\n" + "Paper");
             return "Paper"
-            break;
         case 2:
+            console.log("Super Smart AI Choose....\n" + "Scissors");
             return "Scissors"
-            break;
     }
 }
 
@@ -29,5 +29,29 @@ function getHumanChoice() {
     return sign;
 }
 
-console.log("Super Smart AI Choose....\n" + getComputerChoice());
-console.log("Human Choose....\n" + getHumanChoice());
+function playRound(computerChoice, humanChoice) {
+
+    if (computerChoice === "Rock" && humanChoice === "Rock") {
+        return "Draw!";
+    } else if (computerChoice === "Rock" && humanChoice === "Paper") {
+        return "You Win!";
+    } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
+        return "You Lose!";
+    } else if (computerChoice === "Scissors" && humanChoice === "Scissors") {
+        return "Draw!";
+    } else if (computerChoice === "Scissors" && humanChoice === "Rock") {
+        return "You win!";
+    } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
+        return "You Lose!";
+    } else if (computerChoice === "Paper" && humanChoice === "Paper") {
+        return "Draw!";
+    } else if (computerChoice === "Paper" && humanChoice === "Scissors") {
+        return "You win!";
+    } else if (computerChoice === "Paper" && humanChoice === "Rock") {
+        return "You Lose!";
+    }
+}
+
+// console.log("Super Smart AI Choose....\n" + getComputerChoice());
+// console.log("Human Choose....\n" + getHumanChoice());
+console.log(playRound(getComputerChoice(), getHumanChoice()));
